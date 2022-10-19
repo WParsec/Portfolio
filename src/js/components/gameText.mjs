6e51858e2aa87fd9c9937113f11e1022c94b36b5
WParsec <91538926+WParsec@users.noreply.github.com>
2022-10-19 11:17:21 +0200

    Creates second text JS

--- src/js/components/gameText.mjs
+++ src/js/components/gameText.mjs
@@ -1,18 +1,33 @@
+
 export function writeText() {
     const text = `
     Hello there!
     I’m Tom, nice to meet me. It seems you found my humble portfolio. Go ahead, snoop around! 
-    If you like what you see, why don’t you contact me for a casual chat?
     `;
+
+    const secondText = `Oh, and if you see a banana somewhere, don't touch it.                            
+        Trust me.`
+
     let current = 0;
+    let current2 = 0;
 
     function write() {
-        const elem = document.querySelector("#textContainer");
-        elem.textContent = elem.textContent + text.charAt(current);
+        const container = document.querySelector("#textContainer");
+        container.textContent += text.charAt(current);
         current++;
         if (current < text.length)
             window.setTimeout(write, 10);
     };
+
+    function writeSecond() {
+        const secondContainer = document.querySelector("#secondTextContainer");
+        secondContainer.textContent += secondText.charAt(current2);
+        current2++;
+        if (current2 < secondText.length) {
+            window.setTimeout(writeSecond, 25);
+        }
+    }
     setTimeout(write, 700);
+    setTimeout(writeSecond, 4000);
 }
 
