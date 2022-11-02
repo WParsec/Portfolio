6b0ded9f8e511ab71cfb8cdbfcc4a4d57e47ccd7
WParsec <91538926+WParsec@users.noreply.github.com>
2022-11-02 23:43:49 +0100

    adds cool toggle feature

--- src/js/components/navToggle.mjs
+++ src/js/components/navToggle.mjs
@@ -2,7 +2,16 @@ export function toggleNav() {
     const toggleBtn = document.querySelector('#toggleBtn');
     const menu = document.querySelector('.collapse-nav');
 
+    let menuOpen = false;
+
     toggleBtn.addEventListener('click', () => {
+        if(!menuOpen) {
+            toggleBtn.classList.add('open');
+            menuOpen = true;
+        } else {
+            toggleBtn.classList.remove('open');
+            menuOpen = false;
+        }
         menu.classList.toggle('show');
         menu.classList.toggle('z-10');
     })
