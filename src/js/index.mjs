108d07fd786168e75784e4fc3b325a8fe3434c82
WParsec <91538926+WParsec@users.noreply.github.com>
2022-11-05 16:55:58 +0100

    Button change

---
+++ src/js/index.mjs
@@ -0,0 +1,6 @@
+const {title = "test", body = "test", media = "test", tags = "test"} = VALID_INPUTS
+console.log(VALID_INPUTS);
+console.log(VALID_INPUTS.body);
+
+
+const VALID_TOKEN = { id: "TOKEN", name: "VALID TOKEN" };
